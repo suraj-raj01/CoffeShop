8068772ffa9e168d9cd6c639c254ef0cdc2bb204
SURAJ_KUMAR <surajk38656@gmail.com>
2025-02-10 17:21:41 +0530

    Frontend-footer

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -71,7 +71,34 @@ const Footer = () => {
 
         <section className=''>
           <MDBRow>
-            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+            {/* <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+              <h5 className='text-uppercase'>Links</h5>
+
+              <ul className='list-unstyled mb-0'>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 4
+                  </a>
+                </li>
+              </ul>
+            </MDBCol> */}
+
+            {/* <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
               <h5 className='text-uppercase'>Links</h5>
 
               <ul className='list-unstyled mb-0'>
@@ -123,9 +150,9 @@ const Footer = () => {
                   </a>
                 </li>
               </ul>
-            </MDBCol>
+            </MDBCol> */}
 
-            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+            {/* <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
               <h5 className='text-uppercase'>Links</h5>
 
               <ul className='list-unstyled mb-0'>
@@ -150,41 +177,13 @@ const Footer = () => {
                   </a>
                 </li>
               </ul>
-            </MDBCol>
-
-            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-              <h5 className='text-uppercase'>Links</h5>
-
-              <ul className='list-unstyled mb-0'>
-                <li>
-                  <a href='#!' className='text-white'>
-                    Link 1
-                  </a>
-                </li>
-                <li>
-                  <a href='#!' className='text-white'>
-                    Link 2
-                  </a>
-                </li>
-                <li>
-                  <a href='#!' className='text-white'>
-                    Link 3
-                  </a>
-                </li>
-                <li>
-                  <a href='#!' className='text-white'>
-                    Link 4
-                  </a>
-                </li>
-              </ul>
-            </MDBCol>
+            </MDBCol> */}
           </MDBRow>
         </section>
       </MDBContainer>
 
       <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2025 Copyright:
-        <a className='text-white' href='https://mdbootstrap.com/'>
+        © 2025 Copyright : <a className='text-white' href='https://coffe-shop-blush.vercel.app'>
           coffeeshop.com
         </a>
       </div>
